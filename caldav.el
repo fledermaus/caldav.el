9f4036d9cff94fd25a1b42270bde8c6d60b31e66
Vivek Dasmohapatra <vivek@collabora.co.uk>
2013-05-22 01:00:29 +0100

    Switch caldav.el to lexical, fix boilerplate indentation

--- caldav.el
+++ caldav.el
@@ -1,31 +1,32 @@
- ;;; caldav.el --- CalDAV support
+;; -*- lexical-binding: t -*-
+;;; caldav.el --- CalDAV support
 
- ;; Copyright © 2013  Vivek Dasmohapatra <vivek@etla.org>
+;; Copyright © 2013  Vivek Dasmohapatra <vivek@etla.org>
 
- ;; Author: Vivek Dasmohapatra <vivek@etla.org>
- ;; Maintainer: Vivek Dasmohapatra <vivek@etla.org>
- ;; Keywords: dav, caldav
+;; Author: Vivek Dasmohapatra <vivek@etla.org>
+;; Maintainer: Vivek Dasmohapatra <vivek@etla.org>
+;; Keywords: dav, caldav
 
- ;; This file is NOT part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
- ;; caldav.el is free software: you can redistribute it and/or modify
- ;; it under the terms of the GNU General Public License as published by
- ;; the Free Software Foundation, either version 3 of the License, or
- ;; (at your option) any later version.
+;; caldav.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
- ;; caldav.el is distributed in the hope that it will be useful,
- ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
- ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- ;; GNU General Public License for more details.
+;; caldav.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
- ;; You should have received a copy of the GNU General Public License
- ;; along with caldav.el.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with caldav.el.  If not, see <http://www.gnu.org/licenses/>.
 
- ;; DAV is in RFC 4791.
+;; DAV is in RFC 4791.
 
- ;;; Commentary:
+;;; Commentary:
 
- ;;; Code:
+;;; Code:
 
 (defconst caldav-ns-caldav "urn:ietf:params:xml:ns:caldav")
 (defconst caldav-ical-node 
