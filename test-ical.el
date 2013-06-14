41dce8bc774fccc09e7e8d88f927a5d757802704
Vivek Dasmohapatra <vivek@collabora.co.uk>
2013-06-14 02:19:54 +0100

    Tests - WIP

--- test-ical.el
+++ test-ical.el
@@ -15,18 +15,38 @@
   (mapc 'test-ical-print-date (icalendar--rr-occurrences event zmap end tz))
   (princ "---------------------------------------------------------------\n"))
 
+(defun test-ical-event-as-string (event zmap &optional tz end)
+  (with-temp-buffer
+    (let ((standard-output (current-buffer)))
+      (test-ical-print-event event zmap tz end)
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(defun test-ical-get-event-unpack-end-time (e)
+  (let ((uid (icalendar--get-event-property e 'X-TEST-DATA)))
+    (and uid (string-match "(unpack-until\\s-+\\(\\S-+\\))" uid)
+         (apply 'encode-time
+          (icalendar--decode-isodatetime (match-string 1 uid) nil t)))))
+
 (defun test-ical-print-events (file)
   (let (ical-data zone-map)
     (setq ical-data (with-temp-buffer (insert-file-contents file)
                                       (goto-char (point-min))
                                       (icalendar--read-element nil nil))
           zone-map  (icalendar--convert-all-timezones ical-data))
-    (mapc (lambda (e)
-            (test-ical-print-event e zone-map))
-          (icalendar--all-events ical-data)) ))
+    (mapc (lambda (e &optional uid end printed)
+            (setq end     (test-ical-get-event-unpack-end-time e)
+                  uid     (icalendar--get-event-property e 'UID)
+                  printed (test-ical-event-as-string e zone-map
+                                                     "Europe/London" end))
+            (with-temp-file (format "data/unpacked/%s.txt" uid)
+              (insert printed)))
+          (icalendar--all-events ical-data))
+    nil))
 
 ;; (test-ical-print-events "./data/rfc2445-15min-x6.ics")
 ;; (test-ical-print-events "./data/rfc2445-january.ics")
 ;; (test-ical-print-events "./data/rfc2445-monthly-pen-penultimate-day.ics")
 ;; (test-ical-print-events "./data/rfc2445-yearly-yeardays.ics")
 ;; (test-ical-print-events "./data/rfc2445-3rd-of-3-for-3-months.ics")
+;; (test-ical-print-events "./data/rfc2445examples.ics")
+;; (test-ical-print-events "./data/rfc2445-byday-byweekno.ics")
